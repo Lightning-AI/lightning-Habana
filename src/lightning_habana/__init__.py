24ffb1f418c7df4735835268b53e4b80f142702f
Jirka <jirka.borovec@seznam.cz>
2023-02-28 20:43:21 +0100

    imports

--- src/lightning_habana/__init__.py
+++ src/lightning_habana/__init__.py
@@ -2,17 +2,10 @@
 
 import os
 
-from lightning_utilities.core.imports import package_available
-
-from lightning_habana.__about__ import *  # noqa: F401, F403
-from lightning_habana.accelerator import HPUAccelerator
-from lightning_habana.plugins.io_plugin import HPUCheckpointIO
-from lightning_habana.plugins.precision import HPUPrecisionPlugin
-from lightning_habana.strategies.parallel import HPUParallelStrategy
-from lightning_habana.strategies.single import SingleHPUStrategy
-
 _PACKAGE_ROOT = os.path.dirname(__file__)
 _PROJECT_ROOT = os.path.dirname(_PACKAGE_ROOT)
+
+from lightning_utilities.core.imports import package_available  # noqa: E402
 
 _HABANA_FRAMEWORK_AVAILABLE = package_available("habana_frameworks")
 if _HABANA_FRAMEWORK_AVAILABLE:
@@ -21,5 +14,12 @@ if _HABANA_FRAMEWORK_AVAILABLE:
     _HPU_AVAILABLE = is_habana_available()
 else:
     _HPU_AVAILABLE = False
+
+from lightning_habana.__about__ import *  # noqa: E402, F401, F403
+from lightning_habana.accelerator import HPUAccelerator  # noqa: E402
+from lightning_habana.plugins.io_plugin import HPUCheckpointIO  # noqa: E402
+from lightning_habana.plugins.precision import HPUPrecisionPlugin  # noqa: E402
+from lightning_habana.strategies.parallel import HPUParallelStrategy  # noqa: E402
+from lightning_habana.strategies.single import SingleHPUStrategy  # noqa: E402
 
 __all__ = ["HPUAccelerator", "HPUParallelStrategy", "SingleHPUStrategy", "HPUPrecisionPlugin", "HPUCheckpointIO"]
--- tests/__init__.py
+++ tests/__init__.py
@@ -6,3 +6,7 @@ _TEST_ROOT = os.path.dirname(__file__)
 _PROJECT_ROOT = os.path.dirname(_TEST_ROOT)
 _TEMP_PATH = os.path.join(_PROJECT_ROOT, "test_temp")
 _PATH_DATASETS = os.path.join(_PROJECT_ROOT, "Datasets")
+
+from lightning_habana import _HPU_AVAILABLE  # noqa: E402
+
+assert _HPU_AVAILABLE
