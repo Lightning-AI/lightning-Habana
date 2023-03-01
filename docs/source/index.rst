af612e32e393c9d80832d5eedf49c00929f99f91
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2023-03-01 17:34:39 +0100

    porting HPU integration (#1)
     + code
     + tests
     + docs
     + examples
     + ci

--- docs/source/index.rst
+++ docs/source/index.rst
@@ -3,8 +3,38 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-lightning-habana documentation
-===============================
+lightning + Habana documentation
+================================
+
+.. raw:: html
+
+    <div class="display-card-container">
+        <div class="row">
+
+.. Add callout items below this line
+
+.. displayitem::
+   :header: Basic
+   :description: Learn the basics of single and multi-HPU core training.
+   :col_css: col-md-4
+   :button_link: basic.html
+   :height: 150
+   :tag: basic
+
+.. displayitem::
+   :header: Intermediate
+   :description: Enable state-of-the-art scaling with advanced mix-precision settings.
+   :col_css: col-md-4
+   :button_link: intermediate.html
+   :height: 150
+   :tag: intermediate
+
+.. raw:: html
+
+        </div>
+    </div>
+
+
 .. toctree::
    :maxdepth: 1
    :name: start
--- src/lightning_habana/__init__.py
+++ src/lightning_habana/__init__.py
@@ -2,7 +2,24 @@
 
 import os
 
-from lightning_habana.__about__ import *  # noqa: F401, F403
-
 _PACKAGE_ROOT = os.path.dirname(__file__)
 _PROJECT_ROOT = os.path.dirname(_PACKAGE_ROOT)
+
+from lightning_utilities.core.imports import package_available  # noqa: E402
+
+_HABANA_FRAMEWORK_AVAILABLE = package_available("habana_frameworks")
+if _HABANA_FRAMEWORK_AVAILABLE:
+    from habana_frameworks.torch.utils.library_loader import is_habana_available
+
+    _HPU_AVAILABLE: bool = is_habana_available()
+else:
+    _HPU_AVAILABLE = False
+
+from lightning_habana.__about__ import *  # noqa: E402, F401, F403
+from lightning_habana.accelerator import HPUAccelerator  # noqa: E402
+from lightning_habana.plugins.io_plugin import HPUCheckpointIO  # noqa: E402
+from lightning_habana.plugins.precision import HPUPrecisionPlugin  # noqa: E402
+from lightning_habana.strategies.parallel import HPUParallelStrategy  # noqa: E402
+from lightning_habana.strategies.single import SingleHPUStrategy  # noqa: E402
+
+__all__ = ["HPUAccelerator", "HPUParallelStrategy", "SingleHPUStrategy", "HPUPrecisionPlugin", "HPUCheckpointIO"]
