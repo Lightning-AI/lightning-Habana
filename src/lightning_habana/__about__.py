413276da1cf63d22ad53db7c58d7b9e0e5ceea68
Jerome Anand <88475913+jerome-habana@users.noreply.github.com>
2023-04-13 14:09:27 +0530

    Update src/lightning_habana/__about__.py
    Co-authored-by: Jirka Borovec <6035284+Borda@users.noreply.github.com>

--- src/lightning_habana/__about__.py
+++ src/lightning_habana/__about__.py
@@ -1,43 +1,4 @@
-# Copyright (c) 2023 Habana Labs, Ltd. an Intel Company
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-import os
-import sys
-
-
-def get_version() -> str:
-    """Construct the release version and returns it."""
-    version = os.getenv("RELEASE_VERSION")
-    if not version:
-        version = "0.0.0"
-    build_number = os.getenv("RELEASE_BUILD_NUMBER")
-    if build_number:
-        rval = version + "." + build_number
-    else:
-        try:
-            import subprocess
-
-            root = os.environ["PYTORCH_MODULES_ROOT_PATH"]
-            sha = subprocess.check_output(["git", "-C", root, "rev-parse", "--short", "HEAD"]).decode("ascii").strip()
-            rval = f"{version}+git{sha}"
-        except Exception as e:
-            print(f"Error getting version: {e}", file=sys.stderr)
-            rval = f"{version}+unknown"
-    return rval
-
-
-__version__ = get_version()
+__version__ = "0.2.0dev"
 __author__ = "Lightning-AI et al."
 __author_email__ = "name@lightning.ai"
 __license__ = "Apache-2.0"
