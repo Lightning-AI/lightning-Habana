f44aed5c95bcf5b30689b41154a2e3c9f2298486
Ankit Gola <agola@habana.ai>
2023-04-24 10:51:17 +0300

    1. Use tmpdir as profiler.dirpath in all tests 2. Modify dist tests for above change.
    Signed-off-by: Ankit Gola <agola@habana.ai>

--- tests/test_pytorch/test_profiler.py
+++ tests/test_pytorch/test_profiler.py
@@ -15,10 +15,8 @@
 import glob
 import json
 import os
-import shutil
 
 import pytest
-import torch.distributed
 from lightning_utilities import module_available
 
 if module_available("lightning"):
@@ -48,8 +46,8 @@ def get_device_count(pytestconfig):
     if not hpus:
         assert HPUAccelerator.auto_device_count() >= 1
         return 1
-    assert hpus <= HPUAccelerator.auto_device_count(), "More hpu devices asked than present"
-    assert hpus == 1 or hpus % 8 == 0
+    assert hpus <= HPUAccelerator.auto_device_count(
+    ), "More hpu devices asked than present"
     return hpus
 
 
@@ -62,7 +60,7 @@ def _check_distributed(get_device_count):
 def test_hpu_simple_profiler_instances(get_device_count):
     trainer = Trainer(
         profiler="simple",
-        accelerator="hpu",
+        accelerator=HPUAccelerator(),
         devices=get_device_count,
         strategy=SingleHPUStrategy() if get_device_count == 1 else HPUParallelStrategy(),
     )
@@ -71,7 +69,7 @@ def test_hpu_simple_profiler_instances(get_device_count):
 
 def test_hpu_simple_profiler_trainer_stages(tmpdir):
     model = BoringModel()
-    profiler = SimpleProfiler(dirpath=os.path.join(tmpdir, "profiler_logs"), filename="profiler")
+    profiler = SimpleProfiler(dirpath=tmpdir, filename="profiler")
     _strategy = SingleHPUStrategy()
     _plugins = [HPUPrecisionPlugin(precision="bf16-mixed")]
     trainer = Trainer(
@@ -89,7 +87,8 @@ def test_hpu_simple_profiler_trainer_stages(tmpdir):
     trainer.predict(model)
 
     actual = set(os.listdir(profiler.dirpath))
-    expected = {f"{stage}-profiler.txt" for stage in ("fit", "validate", "test", "predict")}
+    expected = {
+        f"{stage}-profiler.txt" for stage in ("fit", "validate", "test", "predict")}
     assert actual == expected
     for file in list(os.listdir(profiler.dirpath)):
         assert os.path.getsize(os.path.join(profiler.dirpath, file)) > 0
@@ -98,7 +97,7 @@ def test_hpu_simple_profiler_trainer_stages(tmpdir):
 def test_hpu_advanced_profiler_instances(get_device_count):
     trainer = Trainer(
         profiler="advanced",
-        accelerator="hpu",
+        accelerator=HPUAccelerator(),
         devices=get_device_count,
         strategy=SingleHPUStrategy() if get_device_count == 1 else HPUParallelStrategy(),
     )
@@ -107,7 +106,7 @@ def test_hpu_advanced_profiler_instances(get_device_count):
 
 def test_hpu_advanced_profiler_trainer_stages(tmpdir):
     model = BoringModel()
-    profiler = AdvancedProfiler(dirpath=os.path.join(tmpdir, "profiler_logs"), filename="profiler")
+    profiler = AdvancedProfiler(dirpath=tmpdir, filename="profiler")
     _strategy = SingleHPUStrategy()
     trainer = Trainer(
         profiler=profiler,
@@ -124,21 +123,22 @@ def test_hpu_advanced_profiler_trainer_stages(tmpdir):
     trainer.predict(model)
 
     actual = set(os.listdir(profiler.dirpath))
-    expected = {f"{stage}-profiler.txt" for stage in ("fit", "validate", "test", "predict")}
+    expected = {
+        f"{stage}-profiler.txt" for stage in ("fit", "validate", "test", "predict")}
     assert actual == expected
     for file in list(os.listdir(profiler.dirpath)):
         assert os.path.getsize(os.path.join(profiler.dirpath, file)) > 0
 
 
 @pytest.mark.usefixtures("_check_distributed")
-def test_simple_profiler_distributed_files(tmpdir, get_device_count):
+def test_simple_profiler_trainer_stages_distributed(tmpdir, get_device_count):
     """Ensure the proper files are saved in distributed."""
-    profiler = SimpleProfiler(dirpath="/tmp/profiler_logs", filename="profiler")
+    profiler = SimpleProfiler(dirpath=tmpdir, filename="profiler")
     model = BoringModel()
     trainer = Trainer(
         default_root_dir=tmpdir,
         strategy="hpu_parallel",
-        accelerator="hpu",
+        accelerator=HPUAccelerator(),
         devices=get_device_count,
         profiler=profiler,
         fast_dev_run=True,
@@ -146,42 +146,26 @@ def test_simple_profiler_distributed_files(tmpdir, get_device_count):
     trainer.fit(model)
     trainer.validate(model)
     trainer.test(model)
+    trainer.predict(model)
+
+    actual = set(os.listdir(profiler.dirpath))
     expected = {
-        f"{stage}-profiler-{rank}.txt"
-        for stage in ("fit", "validate", "test")
-        for rank in range(0, trainer.num_devices)
-    }
-    # Wait till all hpus have finished.
-    # assert fail before a torch.distributed.barrier() keeps the barrier waiting
-    # on a process that has returned, causing the test to hang.
-    # Do assert checks after the torch.distributed.barrier()
-    assert_fail = ""
-    torch.distributed.barrier()
-    try:
-        if trainer.local_rank == 0:
-            actual = set(os.listdir(profiler.dirpath))
-            assert actual == expected
-            for profilerfile in os.listdir(profiler.dirpath):
-                with open(os.path.join(profiler.dirpath, profilerfile), encoding="utf-8") as pf:
-                    assert len(pf.read()) != 0
-    except AssertionError as ae:
-        assert_fail = ae
-    finally:
-        torch.distributed.barrier()
-        assert assert_fail == ""
-        if trainer.local_rank == 0:
-            shutil.rmtree("/tmp/profiler_logs", ignore_errors=True)
+        f"{stage}-profiler-{trainer.local_rank}.txt" for stage in ("fit", "validate", "test", "predict")}
+    assert actual == expected
+    for profilerfile in os.listdir(trainer.profiler.dirpath):
+        with open(os.path.join(trainer.profiler.dirpath, profilerfile), encoding="utf-8") as pf:
+            assert len(pf.read()) != 0
 
 
 @pytest.mark.usefixtures("_check_distributed")
-def test_advanced_profiler_distributed_files(tmpdir, get_device_count):
+def test_advanced_profiler_trainer_stages_distributed(tmpdir, get_device_count):
     """Ensure the proper files are saved in distributed."""
     model = BoringModel()
-    profiler = AdvancedProfiler(dirpath="/tmp/profiler_logs", filename="profiler")
+    profiler = AdvancedProfiler(dirpath=tmpdir, filename="profiler")
     trainer = Trainer(
         default_root_dir=tmpdir,
         strategy="hpu_parallel",
-        accelerator="hpu",
+        accelerator=HPUAccelerator(),
         devices=get_device_count,
         profiler=profiler,
         fast_dev_run=True,
@@ -189,37 +173,22 @@ def test_advanced_profiler_distributed_files(tmpdir, get_device_count):
     trainer.fit(model)
     trainer.validate(model)
     trainer.test(model)
+    trainer.predict(model)
+
+    actual = set(os.listdir(profiler.dirpath))
     expected = {
-        f"{stage}-profiler-{rank}.txt"
-        for stage in ("fit", "validate", "test")
-        for rank in range(0, trainer.num_devices)
-    }
-    # Wait till all hpus have finished.
-    # assert fail before a torch.distributed.barrier() keeps the barrier waiting
-    # on a process that has returned, causing the test to hang.
-    # Do assert checks after the torch.distributed.barrier()
-    assert_fail = ""
-    torch.distributed.barrier()
-    try:
-        if trainer.local_rank == 0:
-            actual = set(os.listdir(profiler.dirpath))
-            assert actual == expected
-            for profilerfile in os.listdir(profiler.dirpath):
-                with open(os.path.join(profiler.dirpath, profilerfile), encoding="utf-8") as pf:
-                    assert len(pf.read()) != 0
-    except AssertionError as ae:
-        assert_fail = ae
-    finally:
-        torch.distributed.barrier()
-        assert assert_fail == ""
-        if trainer.local_rank == 0:
-            shutil.rmtree("/tmp/profiler_logs", ignore_errors=True)
+        f"{stage}-profiler-{trainer.local_rank}.txt" for stage in ("fit", "validate", "test", "predict")}
+    assert actual == expected
+    for profilerfile in os.listdir(trainer.profiler.dirpath):
+        with open(os.path.join(trainer.profiler.dirpath, profilerfile), encoding="utf-8") as pf:
+            assert len(pf.read()) != 0
 
 
 def test_hpu_profiler_no_string_instances():
     with pytest.raises(MisconfigurationException) as e_info:
         Trainer(profiler="hpu", accelerator="hpu", devices=1)
-    assert "it can only be one of ['simple', 'advanced', 'pytorch', 'xla']" in str(e_info)
+    assert "it can only be one of ['simple', 'advanced', 'pytorch', 'xla']" in str(
+        e_info)
 
 
 def test_hpu_trace_event_cpu_op(tmpdir):
@@ -227,7 +196,7 @@ def test_hpu_trace_event_cpu_op(tmpdir):
     model = BoringModel()
 
     trainer = Trainer(
-        accelerator="hpu",
+        accelerator=HPUAccelerator(),
         devices=1,
         strategy=SingleHPUStrategy(),
         default_root_dir=tmpdir,
@@ -263,7 +232,7 @@ def test_hpu_trace_event_runtime(tmpdir):
     model = BoringModel()
 
     trainer = Trainer(
-        accelerator="hpu",
+        accelerator=HPUAccelerator(),
         devices=1,
         strategy=SingleHPUStrategy(),
         default_root_dir=tmpdir,
@@ -297,7 +266,7 @@ def test_hpu_trace_event_kernel(tmpdir):
     # Run model and prep json
     model = BoringModel()
     trainer = Trainer(
-        accelerator="hpu",
+        accelerator=HPUAccelerator(),
         devices=1,
         strategy=SingleHPUStrategy(),
         default_root_dir=tmpdir,
