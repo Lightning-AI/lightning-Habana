41e7e9bf5dc910f769c81e6864dabcef933e9a47
Jerome <janand@habana.ai>
2023-05-02 14:22:38 +0300

    Debug pipeline failure
    Signed-off-by: Jerome <janand@habana.ai>

--- src/lightning_habana/pytorch/strategies/deepspeed.py
+++ src/lightning_habana/pytorch/strategies/deepspeed.py
@@ -407,8 +407,8 @@ class HPUDeepSpeedStrategy(HPUParallelStrategy):
         model_parameters = filter(lambda p: p.requires_grad, model.parameters())
         deepspeed_engine, deepspeed_optimizer, _, _ = deepspeed.initialize(
             args=argparse.Namespace(
-                device_rank=self.local_rank, use_hpu=True
-            ),  # self.root_device.index), #TBD : check device index usage
+                use_hpu=True
+            ),
             config=self.config,
             model=model,
             model_parameters=model_parameters,
@@ -553,8 +553,8 @@ class HPUDeepSpeedStrategy(HPUParallelStrategy):
 
         model, _, _, _ = deepspeed.initialize(
             args=argparse.Namespace(
-                device_rank=self.local_rank, use_hpu=True
-            ),  # self.root_device.index),  #TBD : check device index usage
+                use_hpu=True
+            ),
             config=inference_config,
             model=model,
             optimizer=None,
