2847a1f723f92fbf3ddf2066515dbc8a3364ca40
Jerome Anand <88475913+jerome-habana@users.noreply.github.com>
2023-08-24 16:59:22 +0530

    Update precision.py

--- src/lightning_habana/fabric/plugins/precision.py
+++ src/lightning_habana/fabric/plugins/precision.py
@@ -58,7 +58,7 @@ class HPUPrecision(Precision):
         verbose: bool = False,
     ) -> None:
         rank_zero_info(
-            "The 'HPUPrecision' strategy is deprecated and will be removed in lightning-habana release 1.12.0,"
+            "The 'HMP' support is deprecated and will be removed in lightning-habana release 1.2.0,"
             " Use 'torch autocast' instead."
         )
 
