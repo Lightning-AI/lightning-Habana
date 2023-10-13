87dd60e5520691894db239d552980ff3bee2d920
Jerome Anand <88475913+jerome-habana@users.noreply.github.com>
2023-10-13 12:50:33 +0530

    Apply suggestions from code review
    Co-authored-by: Jirka Borovec <6035284+Borda@users.noreply.github.com>

--- tests/test_pytorch/test_profiler.py
+++ tests/test_pytorch/test_profiler.py
@@ -184,7 +184,7 @@ def test_hpu_profiler_no_string_instances():
     assert "it can only be one of ['simple', 'advanced', 'pytorch', 'xla']" in str(e_info)
 
 
-@pytest.mark.skip(reason="TBD: Resolve issues with lightning 2.1")
+@pytest.mark.xfail(reason="TBD: Resolve issues with lightning 2.1")
 def test_hpu_trace_event_cpu_op(tmpdir):
     # Run model and prep json
     model = BoringModel()
@@ -221,7 +221,7 @@ def test_hpu_trace_event_cpu_op(tmpdir):
             assert event_duration >= 0
 
 
-@pytest.mark.skip(reason="TBD: Resolve issues with lightning 2.1")
+@pytest.mark.xfail(reason="TBD: Resolve issues with lightning 2.1")
 def test_hpu_trace_event_runtime(tmpdir):
     # Run model and prep json
     model = BoringModel()
@@ -257,7 +257,7 @@ def test_hpu_trace_event_runtime(tmpdir):
             assert event_duration >= 0
 
 
-@pytest.mark.skip(reason="TBD: Resolve issues with lightning 2.1")
+@pytest.mark.xfail(reason="TBD: Resolve issues with lightning 2.1")
 def test_hpu_trace_event_kernel(tmpdir):
     # Run model and prep json
     model = BoringModel()
