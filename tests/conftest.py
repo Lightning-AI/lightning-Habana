fc9f8511bbbe68e649263045b624dcb61a711a18
Ankit Gola <agola@habana.ai>
2023-11-30 13:08:53 +0200

    Teardown of dist pg after tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,7 +14,7 @@
 from pathlib import Path
 
 import pytest
-
+import torch
 from tests import _PATH_DATASETS
 
 
@@ -30,3 +30,10 @@ def pytest_addoption(parser):
 @pytest.fixture()
 def hpus(request):
     return request.config.getoption("--hpus")
+
+@pytest.fixture(autouse=True)
+def teardown_process_group():
+    """Ensures that the distributed process group gets closed before the next test runs."""
+    yield
+    if torch.distributed.is_available() and torch.distributed.is_initialized():
+        torch.distributed.destroy_process_group()
