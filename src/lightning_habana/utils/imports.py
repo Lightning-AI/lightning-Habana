3a17057af3719ceb8b8c5ac29ca72c87b20d6bf8
jyothi kumar sambolu <jsambolu@habana.ai>
2024-05-03 09:48:47 +0300

    Fixing version check with use of base version
    Signed-off-by: jyothi kumar sambolu <jsambolu@habana.ai>

--- src/lightning_habana/utils/imports.py
+++ src/lightning_habana/utils/imports.py
@@ -27,8 +27,8 @@ _TORCH_GREATER_EQUAL_2_0_0 = compare_version("torch", operator.ge, "2.0.0")
 _LIGHTNING_GREATER_EQUAL_2_0_0 = compare_version("lightning", operator.ge, "2.0.0") or compare_version(
     "pytorch_lightning", operator.ge, "2.0.0"
 )
-_LIGHTNING_GREATER_EQUAL_2_3_0 = compare_version("lightning", operator.ge, "2.3.0") or compare_version(
-    "pytorch_lightning", operator.ge, "2.3.0"
+_LIGHTNING_GREATER_EQUAL_2_3_0 = compare_version("lightning", operator.ge, "2.3.0", True) or compare_version(
+    "pytorch_lightning", operator.ge, "2.3.0", True
 )
 _TORCHVISION_AVAILABLE = RequirementCache("torchvision")
 _KINETO_AVAILABLE = torch.profiler.kineto_available()
