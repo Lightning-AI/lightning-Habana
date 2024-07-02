3e1a2531691f1edd3b495638a779e8b52b797c66
Jerome <janand@habana.ai>
2024-07-02 18:29:50 +0300

    Update lightning & deepspeed versions
    Signed-off-by: Jerome <janand@habana.ai>

--- src/lightning_habana/pytorch/strategies/deepspeed.py
+++ src/lightning_habana/pytorch/strategies/deepspeed.py
@@ -82,8 +82,8 @@ log = logging.getLogger(__name__)
 warning_cache = WarningCache()
 
 _HPU_DEEPSPEED_AVAILABLE = (
-    # HPU deep speed is supported only through this pip install git+https://github.com/HabanaAI/DeepSpeed.git@1.16.2
-    RequirementCache("deepspeed==0.14.0+hpu.synapse.v1.16.2")
+    # HPU deep speed is supported only through this pip install git+https://github.com/HabanaAI/DeepSpeed.git@1.16.1
+    RequirementCache("deepspeed==0.14.0+hpu.synapse.v1.16.1")
 )
 if TYPE_CHECKING and _HPU_DEEPSPEED_AVAILABLE:
     import deepspeed
@@ -298,7 +298,7 @@ class HPUDeepSpeedStrategy(HPUDDPStrategy):
         if not _HPU_DEEPSPEED_AVAILABLE:
             raise MisconfigurationException(
                 "To use the `HPUDeepSpeedStrategy`, you must have hpu DeepSpeed installed."
-                " Install it by running `pip install git+https://github.com/HabanaAI/DeepSpeed.git@1.16.2`."
+                " Install it by running `pip install git+https://github.com/HabanaAI/DeepSpeed.git@1.16.1`."
             )
 
         super().__init__(
