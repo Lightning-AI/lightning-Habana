ac60dea4300992c5c9a617bdf7067b00ec1f688a
jyothi kumar sambolu <jsambolu@habana.ai>
2024-08-07 20:38:25 +0300

    Updated tests as per synapse version
    Signed-off-by: jyothi kumar sambolu <jsambolu@habana.ai>

--- tests/test_fabric/test_fsdp.py
+++ tests/test_fabric/test_fsdp.py
@@ -151,7 +151,7 @@ def test_fsdp_train(arg_hpus):
     trainer.run()
 
 
-@pytest.mark.standalone()
+@pytest.mark.xfail(run=False, reason="Saving checkpoint is not fully supported.")
 @pytest.mark.parametrize("manual_wrapping", [True, False])
 def test_train_save_load(tmp_path, arg_hpus, manual_wrapping):
     """Test FSDP training, saving and loading with different wrapping and precision settings."""
